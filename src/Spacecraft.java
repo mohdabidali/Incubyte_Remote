3f8832b7c65f2fca834417b929b102f8eb1de3e6
Mohd Abid <abid@kayatech.com>
2024-02-11 15:42:15 +0530

    Restructure Code

--- src/Spacecraft.java
+++ src/Spacecraft.java
@@ -1,58 +1,65 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SpaceCraft {
-    public static Map<String, Integer> controlSpacecraft(Map<String, Integer> initialPosition, String initialDirection, String[] commands) {
-        String[] directions = {"N", "E", "S", "W", "Up", "Down"};
-        Map<String, Integer> position = new HashMap<>(initialPosition);
-        int directionIndex = Arrays.asList(directions).indexOf(initialDirection);
+public class Main {
+    private static final Map<Character, Integer> DIRECTIONS_MAP = new HashMap<>(); // Mapping of direction characters to indices
 
-        for (String command : commands) {
+    static {
+        DIRECTIONS_MAP.put('N', 0);
+        DIRECTIONS_MAP.put('E', 1);
+        DIRECTIONS_MAP.put('S', 2);
+        DIRECTIONS_MAP.put('W', 3);
+        DIRECTIONS_MAP.put('U', 4);
+        DIRECTIONS_MAP.put('D', 5);
+    }
+
+    public static Map<String, Integer> controlSpacecraft(Map<String, Integer> initialPosition, char initialDirection, char[] commands) {
+        Map<String, Integer> position = new HashMap<>(initialPosition);
+        int directionIndex = DIRECTIONS_MAP.get(initialDirection);
+
+        for (char command : commands) {
             switch (command) {
-                case "f":
+                case 'f':
                     position.put("z", position.get("z") + (directionIndex == 4 ? -1 : directionIndex == 5 ? 1 : 0));
                     break;
-                case "b":
+                case 'b':
                     position.put("z", position.get("z") - (directionIndex == 4 ? -1 : directionIndex == 5 ? 1 : 0));
                     break;
-                case "l":
-                    directionIndex = (directionIndex + 1) % directions.length;
+                case 'l':
+                    directionIndex = (directionIndex + 1) % DIRECTIONS_MAP.size();
                     break;
-                case "r":
-                    directionIndex = (directionIndex - 1 + directions.length) % directions.length;
+                case 'r':
+                    directionIndex = (directionIndex - 1 + DIRECTIONS_MAP.size()) % DIRECTIONS_MAP.size();
                     break;
-                case "u":
-                    if (!directions[directionIndex].equals("Up")) {
+                case 'u':
+                    if (DIRECTIONS_MAP.get(initialDirection) != 'D') {
                         position.put("y", position.get("y") + 1);
                     }
                     break;
-                case "d":
-                    if (!directions[directionIndex].equals("Down")) {
+                case 'd':
+                    if (DIRECTIONS_MAP.get(initialDirection) != 'U') {
                         position.put("y", position.get("y") - 1);
                     }
                     break;
             }
         }
 
-        Map<String, Integer> result = new HashMap<>();
-        result.putAll(position);
-        result.put("direction", directionIndex); // Store direction index
+        Map<String, Integer> result = new HashMap<>(position);
+        result.put("direction", (int) initialDirection); // Store initial direction character
 
         return result;
     }
 
     public static void main(String[] args) {
-        String[] directions = {"N", "E", "S", "W", "Up", "Down"}; // Declare directions array
         Map<String, Integer> startPosition = new HashMap<>();
         startPosition.put("x", 0);
         startPosition.put("y", 0);
         startPosition.put("z", 0);
-        String startDirection = "N";
-        String[] commandList = {"f", "r", "u", "b", "l"};
+        char startDirection = 'N';
+        char[] commandList = {'f', 'r', 'u', 'b', 'l'};
 
         Map<String, Integer> result = controlSpacecraft(startPosition, startDirection, commandList);
         System.out.println("Final Position: (" + result.get("x") + ", " + result.get("y") + ", " + result.get("z") + ")");
-        System.out.println("Final Direction: " + directions[result.get("direction")]);
+        System.out.println("Final Direction: " + (char) result.get("direction").intValue());
     }
 }
