ae08275b1ac8701199ba5d5fe660abb2dd0228e9
Mohd Abid <abid@kayatech.com>
2024-02-11 15:39:12 +0530

    Correct Output

--- src/Spacecraft.java
+++ src/Spacecraft.java
@@ -1,175 +1,58 @@
-public class Main {
-    private int x;
-    private int y;
-    private int z;
-    private String direction;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Main(int x, int y, int z, String direction) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.direction = direction;
-    }
+public class SpaceCraft {
+    public static Map<String, Integer> controlSpacecraft(Map<String, Integer> initialPosition, String initialDirection, String[] commands) {
+        String[] directions = {"N", "E", "S", "W", "Up", "Down"};
+        Map<String, Integer> position = new HashMap<>(initialPosition);
+        int directionIndex = Arrays.asList(directions).indexOf(initialDirection);
 
-    public void moveForward() {
-        switch (direction) {
-            case "N":
-                y++;
-                break;
-            case "S":
-                y--;
-                break;
-            case "E":
-                x++;
-                break;
-            case "W":
-                x--;
-                break;
-            case "Up":
-                z++;
-                break;
-            case "Down":
-                z--;
-                break;
-        }
-    }
-
-    public void moveBackward() {
-        switch (direction) {
-            case "N":
-                y--;
-                break;
-            case "S":
-                y++;
-                break;
-            case "E":
-                x--;
-                break;
-            case "W":
-                x++;
-                break;
-            case "Up":
-                z--;
-                break;
-            case "Down":
-                z++;
-                break;
-        }
-    }
-
-    public void turnLeft() {
-        switch (direction) {
-            case "N":
-                direction = "W";
-                break;
-            case "S":
-                direction = "E";
-                break;
-            case "E":
-                direction = "N";
-                break;
-            case "W":
-                direction = "S";
-                break;
-        }
-    }
-
-    public void turnRight() {
-        switch (direction) {
-            case "N":
-                direction = "E";
-                break;
-            case "S":
-                direction = "W";
-                break;
-            case "E":
-                direction = "S";
-                break;
-            case "W":
-                direction = "N";
-                break;
-        }
-    }
-
-    public void turnUp() {
-        switch (direction) {
-            case "N":
-            case "E":
-                direction = "Up";
-                break;
-            case "S":
-            case "W":
-                direction = "Down";
-                break;
-        }
-    }
-
-    public void turnDown() {
-        switch (direction) {
-            case "N":
-            case "E":
-                direction = "Down";
-                break;
-            case "S":
-            case "W":
-                direction = "Up";
-                break;
-        }
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public static void executeCommands(Main spacecraft, char[] commands) {
-        for (char command : commands) {
+        for (String command : commands) {
             switch (command) {
-                case 'f':
-                    spacecraft.moveForward();
+                case "f":
+                    position.put("z", position.get("z") + (directionIndex == 4 ? -1 : directionIndex == 5 ? 1 : 0));
                     break;
-                case 'b':
-                    spacecraft.moveBackward();
+                case "b":
+                    position.put("z", position.get("z") - (directionIndex == 4 ? -1 : directionIndex == 5 ? 1 : 0));
                     break;
-                case 'l':
-                    spacecraft.turnLeft();
+                case "l":
+                    directionIndex = (directionIndex + 1) % directions.length;
                     break;
-                case 'r':
-                    spacecraft.turnRight();
+                case "r":
+                    directionIndex = (directionIndex - 1 + directions.length) % directions.length;
                     break;
-                case 'u':
-                    spacecraft.turnUp();
+                case "u":
+                    if (!directions[directionIndex].equals("Up")) {
+                        position.put("y", position.get("y") + 1);
+                    }
                     break;
-                case 'd':
-                    spacecraft.turnDown();
+                case "d":
+                    if (!directions[directionIndex].equals("Down")) {
+                        position.put("y", position.get("y") - 1);
+                    }
                     break;
             }
         }
+
+        Map<String, Integer> result = new HashMap<>();
+        result.putAll(position);
+        result.put("direction", directionIndex); // Store direction index
+
+        return result;
     }
 
     public static void main(String[] args) {
-        int startX = 0;
-        int startY = 0;
-        int startZ = 0;
+        String[] directions = {"N", "E", "S", "W", "Up", "Down"}; // Declare directions array
+        Map<String, Integer> startPosition = new HashMap<>();
+        startPosition.put("x", 0);
+        startPosition.put("y", 0);
+        startPosition.put("z", 0);
         String startDirection = "N";
-        char[] commands = {'f', 'r', 'u', 'b', 'l'};
+        String[] commandList = {"f", "r", "u", "b", "l"};
 
-        Main chandrayaan3 = new Main(startX, startY, startZ, startDirection);
-        executeCommands(chandrayaan3, commands);
-
-        System.out.println("Final Position: (" + chandrayaan3.getX() + ", " + chandrayaan3.getY() + ", " + chandrayaan3.getZ() + ")");
-        System.out.println("Final Direction: " + chandrayaan3.getDirection());
+        Map<String, Integer> result = controlSpacecraft(startPosition, startDirection, commandList);
+        System.out.println("Final Position: (" + result.get("x") + ", " + result.get("y") + ", " + result.get("z") + ")");
+        System.out.println("Final Direction: " + directions[result.get("direction")]);
     }
 }
-
